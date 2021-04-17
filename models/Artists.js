2d8567399057284e348bab497a707971cc7e2a47
clove18 <35983894+clove18@users.noreply.github.com>
2021-04-16 20:43:56 -0400

    Create Artists.js

---
+++ models/Artists.js
@@ -0,0 +1,24 @@
+export default (sequelize, DataTypes) => {
+    const Artists = sequelize.define(
+      'Artists',
+      {
+        artist_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          unique: true,
+          primaryKey: true
+        },
+        label_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
+        artist_name: {
+          type: DataTypes.STRING
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return Artists;
+  };
