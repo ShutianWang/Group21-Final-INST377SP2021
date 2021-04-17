78798e18ecd7f739a21c8fd1ed7191fb979eddd9
clove18 <35983894+clove18@users.noreply.github.com>
2021-04-16 20:44:02 -0400

    Create AlbumInfo.js

---
+++ models/AlbumInfo.js
@@ -0,0 +1,32 @@
+import Artist from "./Artist";
+
+export default (sequelize, DataTypes) => {
+    const AlbumInfo = sequelize.define(
+      'AlbumInfo',
+      {
+        album_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
+        artist_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true,
+            references: {model: Artist, key: "artist_id"}
+        },
+        album_name: {
+            type: DataTypes.STRING
+        },
+        album_type: {
+            type : DataTypes.STRING
+        },
+        album_release_date: {
+            type: DataTypes.DATE
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return AlbumInfo;
+  };
