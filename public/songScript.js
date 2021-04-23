35b367ed0c747fc76a9d9c39b67bf866108af31a
mattmrss2 <78233113+mattmrss2@users.noreply.github.com>
2021-04-23 18:03:36 -0400

    db nav and song init

---
+++ public/songScript.js
@@ -0,0 +1,16 @@
+
+async function songsTable() {
+    const songs = await fetch("/api/songs");
+    const songsData = await songs.json();
+    const songTable = document.querySelector(".tableBody");
+    console.log(songTable)
+    console.log(songsData);
+    
+    songsData.data.forEach((item) => {
+        console.log(item)
+        const appendItem = document.createElement('tr');
+        appendItem.innerHTML = `<td>${item.song_id}</td><td>${item.song_name}</td><td>${item.song_id}</td>`
+        resTable.append(appendItem)
+    });
+}
+window.onload = diningTable;
