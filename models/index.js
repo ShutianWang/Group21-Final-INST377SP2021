0d3b8592b7c3dfeb25554059fcc9c94db462203f
clover43 <35983894+clover43@users.noreply.github.com>
2021-05-06 17:48:36 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import AlbumInfo from './AlbumInfo.js';
 import Artists from './Artists.js';
 import Genres from './Genres.js'
 import Charting from './Charting.js'
+import Songs from './Songs.js'
 
 export default {
   AlbumInfo,
   Artists,
   Genres,
-  Charting
+  Charting,
+  Songs
 };
